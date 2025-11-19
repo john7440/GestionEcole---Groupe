7805550159fca4055dd97c9806554c9ed7b074ca
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-19 13:58:14 +0100

    Creation de la méthode str de la classe Person

--- Person.py
+++ Person.py
@@ -9,11 +9,21 @@ class Person:
     Classe représentant une person
 
     Attributs :
-        name : nom de la personne
+        firstName : prénom de la personne
+        lastName : nom de la personne
         age : âge de la personne
+        adress : adresse de la personne
     """
 
     firstName: str
     lastName: str
     age: int
     """ adress : Adress"""
+
+
+    def __str__(self) -> str:
+        """
+        Permet de configurer l'affichage d'une personne peu importe son ty
+        :return: Prenom, nom ,age et adresse de la personne
+        """
+        return f"{self.firstName} {self.lastName} {self.age}"
