fd315f88822ccd8492533c22c0e0b1423c69f27d
Rémi Campistron <remi.campistron@gmail.com>
2025-11-19 14:34:37 +0100

    Ajout de students dans Course. #fixes 4

--- course.py
+++ course.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, date
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
+
 from Teacher import Teacher
+from Student import Student
 
 
 @dataclass
@@ -10,3 +13,4 @@ class Course:
     start_date: datetime
     end_date: datetime
     teacher: Teacher
+    students: List[Student] = field(default_factory=list)
