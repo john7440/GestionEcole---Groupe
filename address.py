9988cd2b8b3cfe8e463776724bba118aefaa29d4
Rémi Campistron <remi.campistron@gmail.com>
2025-11-19 14:25:38 +0100

    Ajouter persons_list dans Address. fixes #3

--- address.py
+++ address.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from dataclasses import dataclass
-
+from dataclasses import dataclass, field
+from typing import List,Dict
+import Person
 
 @dataclass
 class Address:
     street: str
     city: str
     zipcode: int
-
+    persons_list: List[Person] = field(default_factory=list)
 
