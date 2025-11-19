a7c98bf73f492772475d85a94d32da08e722cc9e
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-19 14:11:26 +0100

    Creation de la classe Student

---
+++ Student.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from Person import Person
+
+
+@dataclass
+class Student(Person):
+    """
+    Classe représentant un étudiant.
+    """
+
+    id: int
+
+    def __str__(self) -> str:
+        return f"{self.firstName} {self.lastName}, {self.age}, {self.address}, {self.id}"
+
+    def __repr__(self) -> str:
+        return f"Student({self.firstName}, {self.lastName}, {self.age}, {self.address}, {self.id})"
