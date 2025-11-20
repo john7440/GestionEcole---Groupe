187b6b25a23081026633d57de6d1f9eb641108b6
Rémi Campistron <remi.campistron@gmail.com>
2025-11-20 14:01:34 +0100

    créer la méthode __str__ pour afficher une adresse dans la classe Adress. fixes #7

--- address.py
+++ address.py
@@ -10,3 +10,11 @@ class Address:
     zipcode: int
     persons_list: List[person] = field(default_factory=list)
 
+    def __str__(self) -> str:
+        returned_string: str = ""
+        for p in self.persons_list:
+            returned_string += p.firstName + " " + p.lastName + ", "
+
+        returned_string += f"{self.street}, {self.city}, {self.zipcode}"
+        return returned_string
+
