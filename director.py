969e8dc3405a0a00fb269742561fa3d305118864
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-20 10:51:09 +0100

    Ajout de la classe Director

---
+++ director.py
@@ -0,0 +1,52 @@
+
+from dataclasses import dataclass
+from typing import ClassVar, List
+from person import Person
+
+
+@dataclass
+class Student(Person):
+    """
+    Classe représentant un directeur.
+    """
+    id: int
+    students: ClassVar[List['Student']] = []
+
+    def __str__(self) -> str:
+        return f"{self.firstName} {self.lastName}, {self.age}, {self.address}, {self.id}"
+
+    def __repr__(self) -> str:
+        return f"Student({self.firstName}, {self.lastName}, {self.age}, {self.address}, {self.id})"
+
+    @classmethod
+    def create_student(cls, student: 'Student') -> 'Student':
+        """
+        Ajoute un objet Student à la liste des clients.
+        :param student: Instance de Student à ajouter
+        :return: L'objet Student ajouté
+        """
+        cls.students.append(student)
+        return student
+
+    @classmethod
+    def nb_students(cls) -> int:
+        """
+        Permet de l'affichage du nombre d'élèves passé ce jour
+        :return: nombre d'élèves passés ce jour
+        """
+        return len(cls.students)
+
+    @classmethod
+    def print_students(cls) -> None:
+        """
+        Affiche la liste des élèves passés aujourd'hui
+        :return: None
+        """
+        if not cls.students:
+            print("Il n'y a aucun eleve.")
+        else:
+            print(f"\nListe des {Student.nb_students()} eleves passés aujourd'hui :")
+            print("-" * 40)
+            for i, student in enumerate(cls.students, start=1):
+                print(f"{student.id}. {student.firstName} {student.lastName} - {student.age} ans - {student.address}")
+            print("-" * 40)
