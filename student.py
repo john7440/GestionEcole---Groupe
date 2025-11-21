c2cd5c3f69b4fba3b41c3ee045e91ee50b9f9403
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-21 09:17:46 +0100

    Ajout de docstrings dans la classe student

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import ClassVar, List
+from typing import List
 from person import Person
 
 
@@ -12,11 +12,23 @@ class Student(Person):
     courses: List['Course'] = field(default_factory=list)
 
     def __str__(self) -> str:
+        """
+        Retourne une représentation textuelle lisible de l'étudiant.
+        Inclut le prénom, le nom, l'âge, l'adresse, l'ID et
+        la liste des cours suivis.
+        :return: String représentant l'objet Student.
+        """
         cours = ", ".join(course.name for course in self.courses) if self.courses else "Aucun cours"
         return (f"{self.firstName} {self.lastName}, {self.age} ans, "
                 f"{self.address}, ID : {self.id}, Cours : {cours}")
 
     def __repr__(self) -> str:
+        """
+        Retourne une représentation textuelle pour le développement.
+        Inclut le prénom, le nom, l'âge, l'adresse, l'ID et
+        la liste des cours suivis.
+        :return: String représentant l'objet Student.
+        """
         cours = [course.name for course in self.courses]
         return (f"Student({self.firstName}, {self.lastName}, {self.age}, "
                 f"{self.address}, id={self.id}, cours={cours})")
